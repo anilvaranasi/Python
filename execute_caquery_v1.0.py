a6d2a6e7681262cf1527aebf3ffb2ab89456ef48
anilvaranasi_02@yahoo.com <anilvaranasi_02@yahoo.com>
2021-08-01 09:27:52 -0700

    uf

---
+++ execute_caquery_v1.0.py
@@ -0,0 +1,17 @@
+#Location of database https://astra.datastax.com/org/e3643ed3-6da6-4252-9d37-1b22eea7d708/database/66964d1a-482d-4b55-a552-a50a2a148924/connect
+#Location of key   https://astra.datastax.com/org/e3643ed3-6da6-4252-9d37-1b22eea7d708/settings/tokens
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+
+cloud_config= {
+        'secure_connect_bundle': 'D:\ANIL\VSCode\Python\secure-connect-Varanawesomenow.zip'
+}
+client_id='wJlosGCAeWYGLyZBUIlYEKcv'
+client_secret='IsZ8E8C8vHmYLjEEezRdoZIL0ciW1DifpsUH63R,gu3DiypHP47v.3ZkwR3Qd3Hkb4JMZlHALGBUgML5oWaEMfOLGaDRKMNKqlyS+048jHgN+L6_G,QAU9,8rQ78Ati+'
+auth_provider = PlainTextAuthProvider(client_id, client_secret)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect('varanawesome')
+
+rows = session.execute('SELECT name, age, email FROM users')
+for row in rows:
+    print (row.name, row.age, row.email)
