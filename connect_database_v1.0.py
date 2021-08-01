918ffbb3477ea5b0814056e7fbc0da8c2563e420
anilvaranasi_02@yahoo.com <anilvaranasi_02@yahoo.com>
2021-08-01 09:07:55 -0700

    commit on main

--- connect_database_v1.0.py
+++ connect_database_v1.0.py
@@ -4,7 +4,7 @@ from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 
 cloud_config= {
-        'secure_connect_bundle': 'D:\ANIL\VSCode\python_project\secure-connect-Varanawesomenow.zip'
+        'secure_connect_bundle': 'D:\ANIL\VSCode\Python\secure-connect-Varanawesomenow.zip'
 }
 client_id='wJlosGCAeWYGLyZBUIlYEKcv'
 client_secret='IsZ8E8C8vHmYLjEEezRdoZIL0ciW1DifpsUH63R,gu3DiypHP47v.3ZkwR3Qd3Hkb4JMZlHALGBUgML5oWaEMfOLGaDRKMNKqlyS+048jHgN+L6_G,QAU9,8rQ78Ati+'
